08d06844a36c2c08ea9dc4dc96a2b8d8f5d4c8bd
Milkyo <dp221125@naver.com>
2020-05-26 15:52:59 +0900

    Initial Commit

---
+++ SwiftUIWithMovieDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWithMovieDB
+//
+//  Created by Seokho on 2020/05/26.
+//  Copyright © 2020 Seokho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
