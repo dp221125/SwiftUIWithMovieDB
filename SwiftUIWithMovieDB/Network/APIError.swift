2b3c6fb64ce95679eaee2b40dc722dd4714ef743
Milkyo <dp221125@naver.com>
2020-05-26 16:15:04 +0900

    APIError 추가

---
+++ SwiftUIWithMovieDB/Network/APIError.swift
@@ -0,0 +1,14 @@
+//
+//  APIError.swift
+//  SwiftUIWithMovieDB
+//
+//  Created by Seokho on 2020/05/26.
+//  Copyright © 2020 Seokho. All rights reserved.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case parseError
+    case unknownError
+}
